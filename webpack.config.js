7e7cbc76162b72945d0f54aa758ebfc79fe7dea4
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-27 14:37:45 -0700

    (fix) Add NODE_ENV: JSON.stringify(process.env.NODE_ENV) to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        LIVE_URL: process.env.LIVE_URL
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
       }
     })
   ]
