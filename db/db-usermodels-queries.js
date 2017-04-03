2a8200c12ff08ca028cee291c105ce7cf6048f2c
Craig Rodrigues <PagingCraig@gmail.com>
2017-04-02 18:14:36 -0700

    Change addFriendToList query to not add duplicate names

--- db/db-usermodels-queries.js
+++ db/db-usermodels-queries.js
@@ -2,15 +2,7 @@ var models = require('./index.js');
 var users = models.db.collection('usermodels');
 
 module.exports.addFriendToList = function(friend, currentUser) {
-	// return users.findOne({username: currentUser})
-	// 	.then((data) => {
-	// 		if (data.friendList.indexOf(friend) < 0) {
-	// 			users.update({username: currentUser}, {$push: {"friendList": friend}});
-	// 		}
-
-	// 		return data;
-	// 	});
-	return users.findOneAndUpdate({username: currentUser}, {$push: {friendList: friend}}, {returnOriginal: false});
+	return users.findOneAndUpdate({username: currentUser}, {$addToSet: {friendList: friend}}, {returnOriginal: false});
 };
 
 module.exports.selectUserByName = function(username) {
